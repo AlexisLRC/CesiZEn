b99f95e9cb60c007f981880b34e3aa151066a7d1
A.leroch <alexislerocheleuil@gmail.com>
2026-03-02 14:23:37 +0100

    corrgie l'affichage du logo dans connexion et inscription

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,9 +15,11 @@
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
+                <div>
                 <a href="/">
-                    <img src="{{ asset('logo.png') }}" class="w-24 h-auto" alt="Logo CesiZen">
+                    <img src="{{ asset('logo.png') }}" class="w-32 h-auto mix-blend-multiply" alt="Logo CesiZen">
                 </a>
+            </div>
             </div>
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg border-t-4 border-cesi-green">
